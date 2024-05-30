a202d62a12f1dd039a3602104b8c9342babe46a0
nicolas pizarro z <nicolaspizarrozambra@gmail.com>
2024-05-29 20:23:21 -0400

    bind parametros

--- library/DB.php
+++ library/DB.php
@@ -11,6 +11,7 @@ class DB
     private $dsn;
     private $username;
     private $password;
+    private $stmt;
 
     public function __construct()
     {
@@ -32,5 +33,47 @@ class DB
             throw new Exception("Error al conectar a la base de datos" . $e->getMessage());
         }
     }
+
+
+    public function ejecutarQuery(string $query, array $binds = [])
+    {
+        $queryLimpia = preg_replace('/\s+/', ' ', $query);
+        $queryLimpia = trim($queryLimpia);
+
+        // ver si tiene varios parametros a agregar
+        $parametros = $this->parametros($queryLimpia);
+
+        $this->stmt = $this->pdo->prepare($queryLimpia);
+
+        if (count($parametros) > 0) {
+
+            $this->bindParametros($parametros, $binds);
+            $this->stmt->execute();
+        } else {
+            $this->stmt->execute();
+        }
+    }
+
+    public function bindParametros(array $parametros = [], array $binds = [])
+    {
+        $coinciden = count($parametros) == count($binds);
+
+        if ($coinciden) {
+            for ($i = 0; $i < count($parametros); $i++) {
+                $this->stmt->bindParam($parametros[$i], $binds[$i]);
+            }
+        } else {
+            throw new \InvalidArgumentException('El número de parámetros y valores de vinculación no coinciden.');
+        }
+    }
+
+
+    public function parametros(string $query)
+    {
+        $parametros = [];
+        preg_match_all('/:\w+/', $query, $parametros);
+        return $parametros;
+    }
+
 
 }
