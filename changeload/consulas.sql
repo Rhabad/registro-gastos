cad5c6dc843fa3f73d12e889bdb4c339370e2a93
nicolas pizarro z <nicolaspizarrozambra@gmail.com>
2024-05-31 13:52:45 -0400

    cambio nombre, consulta select producto

---
+++ changeload/consulas.sql
@@ -0,0 +1,15 @@
+INSERT INTO
+    producto (
+        nombre_prod,
+        tipo_prod,
+        precio,
+        precio_oferta
+    )
+VALUES (
+        "Audifonos",
+        "electronico",
+        "5000",
+        "3000"
+    );
+
+SELECT * FROM producto;
