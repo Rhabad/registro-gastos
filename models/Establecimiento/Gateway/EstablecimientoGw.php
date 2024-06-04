f169e65e1b3751d24ca4d9847c7688b23fbbb20c
nicolas pizarro z <nicolaspizarrozambra@gmail.com>
2024-06-03 23:39:17 -0400

    listar e insertar en tabla establecimiento comercial

--- models/Establecimiento/Gateway/EstablecimientoGw.php
+++ models/Establecimiento/Gateway/EstablecimientoGw.php
@@ -3,6 +3,7 @@
 namespace Models\Establecimiento\Gateway;
 
 use Library\DB;
+use Models\Establecimiento\Model\Establecimiento;
 
 class EstablecimientoGw
 {
@@ -11,5 +12,27 @@ class EstablecimientoGw
     public function __construct(DB $db)
     {
         $this->db = $db;
+    }
+
+    public function crearEstablecimiento(Establecimiento $establecimiento)
+    {
+        $query = 'INSERT INTO
+            establecimiento_comercial (nombre_establ, zona)
+                VALUES (:nombre_establ, :zona)';
+
+        $this->db->ejecutarQuery(
+            $query,
+            [
+                $establecimiento->getNombreEstabl(),
+                $establecimiento->getZona()
+            ]
+        );
+    }
+
+    public function listarEstablecimiento()
+    {
+        $query = 'SELECT * FROM establecimiento_comercial;';
+
+        $this->db->ejecutarQuery($query);
     }
 }
