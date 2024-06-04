905b067972608866abf1291a9ffa6f6a989b3419
nicolas pizarro z <nicolaspizarrozambra@gmail.com>
2024-06-04 11:00:24 -0400

    listar y agregar tipo producto

--- models/TipoProducto/Gateway/TipoProductoGw.php
+++ models/TipoProducto/Gateway/TipoProductoGw.php
@@ -2,6 +2,7 @@
 namespace Models\TipoProducto\Gateway;
 
 use Library\DB;
+use Models\TipoProducto\Model\TipoProducto;
 
 class TipoProductoGw
 {
@@ -9,5 +10,30 @@ class TipoProductoGw
     public function __construct(DB $db)
     {
         $this->db = $db;
+    }
+
+    public function createTipo(TipoProducto $tipoProducto)
+    {
+        $query = 'INSERT INTO
+        tipo_producto (tipo_prod, descripcion)
+    VALUES (
+            :tipo_prod,
+            :descripcion
+        )';
+
+        $this->db->ejecutarQuery(
+            $query,
+            [
+                $tipoProducto->getTipoProd(),
+                $tipoProducto->getDescripcion()
+            ]
+        );
+    }
+
+    public function listarTipo()
+    {
+        $query = 'SELECT * FROM tipo_producto';
+
+        return $this->db->ejecutarQuery($query);
     }
 }
