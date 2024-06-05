fdada55df69305cc3430e544229f42f38acd19cb
nicolas pizarro z <nicolaspizarrozambra@gmail.com>
2024-06-05 14:43:23 -0400

    la query

--- models/Registro/Gateway/RegistroGw.php
+++ models/Registro/Gateway/RegistroGw.php
@@ -34,4 +34,28 @@ class RegistroGw
             ]
         );
     }
+
+    public function listarRegistros()
+    {
+        $query = 'SELECT
+        r.id_registro as id,
+        p.nombre_prod as producto,
+        tp.tipo_prod as tipo,
+        p.precio,
+        p.precio_oferta as oferta,
+        r.cantidad,
+        r.fecha_compra as fecha_registro,
+        CONCAT(
+            ec.zona,
+            " - ",
+            ec.nombre_establ
+        ) as zona
+    FROM
+        registro r
+        LEFT JOIN producto p ON r.prod_id = p.id_producto
+        LEFT JOIN tipo_producto tp ON p.tipo_prod_id = tp.id_tipo_prod
+        LEFT JOIN establecimiento_comercial ec ON r.establ_id = ec.id_establ_com';
+
+        return $this->db->ejecutarQuery($query);
+    }
 }
